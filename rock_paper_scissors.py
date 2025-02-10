fec885319305e263a410fdcedfca286b28395114
JKariithi <janicekariithi@gmail.com>
2025-02-10 18:51:36 +0300

    Modularizing the code. That is breaking the code down into smaller parts.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,25 +1,30 @@
 # Rock paper Scissors Game
 from numpy import random
 
-#using a loop for the Game
-while True:
-    emojis = {
+
+emojis = {
         'r': '🪨',
         'p': '📄',
         's': '✂️'
     }
-    choices = ('r','p','s')
-    user_choice = input("Rock, paper or Scissors (r/p/s) ?: ").lower()
-    if  user_choice not in choices:
-        print("Invalid choice") # better way of handling the error
-        continue
-    
-    computer_choice = random.choice(choices)
+choices = ('r','p','s')
 
-    #Printing out the choices of the user and computer
+#Users choice fn
+def get_user_choice():
+    while True:
+      user_choice = input("Rock, paper or Scissors (r/p/s) ?: ").lower()
+      if  user_choice in choices:
+          return user_choice
+      else:
+          print("Invalid choice") # better way of handling the error
+
+# Display choices
+def display_choices(user_choice, computer_choice):
     print(f"Your chose {emojis[user_choice]}")
     print(f"Computer chose {emojis[computer_choice]}")
 
+#Determine the winner
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Tie!")
     elif (user_choice == 'r' and computer_choice == 's') or (user_choice == 's' and computer_choice == 'p') or (user_choice == 'p' and computer_choice == 'r'):
@@ -27,11 +32,21 @@ while True:
     else:
         print("You lose!")
 
-    # ask if they want to continue
-    continue_playing = input("Continue (y/n) ?: ").lower()
-    if continue_playing == 'n':
+def playing_game():
+ while True:
+   user_choice = get_user_choice()
+
+   computer_choice = random.choice(choices)
+
+   display_choices(user_choice, computer_choice)
+
+   determine_winner(user_choice,computer_choice)
+
+   continue_playing = input("Continue (y/n) ?: ").lower()
+   if continue_playing == 'n':
         break
 
+playing_game()
 
 
 
